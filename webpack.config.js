d32d8901a365ed286e3a79c7abb6fa6ea03b64e3
n199603 <yutonakamura0320@gmail.com>
2021-01-27 09:47:40 +0900

    TypeScript導入

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,27 @@
 const path = require('path')
 
 module.exports = {
-  entry: './src/index.js',
+  // 最初に読み込むファイル
+  entry: './src/index.ts',
+  module: {
+    // ビルド時に追加で行う処理
+    rules: [
+      {
+        // .tsファイルにts-loaderを実行
+        test: /\.ts$/,
+        use: 'ts-loader',
+        // 除外するファイル
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  // 解決するファイルの拡張子
+  resolve: {
+    extensions: ['.ts'],
+  },
+  // 出力するファイル
   output: {
+    // distディレクトリに対してindex.jsを出力
     path: path.resolve(__dirname, 'dist'),
     filename: 'index.js',
     publicPath: 'dist/',
