de8e48da4e04f3af4e9283a0d84cd2618b96cf26
n199603 <yutonakamura0320@gmail.com>
2021-01-27 21:13:36 +0900

    ビュー作成

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,14 @@ const path = require('path')
 
 module.exports = {
   // 最初に読み込むファイル
-  entry: './src/index.ts',
+  entry: './src/index.tsx',
   module: {
     // ビルド時に追加で行う処理
     rules: [
       {
-        // .tsファイルにts-loaderを実行
-        test: /\.ts$/,
+        // .tsxファイルにts-loaderを実行
+        // x?→xの有無は任意(.tsに対しても適用する)
+        test: /\.tsx?$/,
         use: 'ts-loader',
         // 除外するファイル
         exclude: /node_modules/,
@@ -17,7 +18,7 @@ module.exports = {
   },
   // 解決するファイルの拡張子
   resolve: {
-    extensions: ['.ts'],
+    extensions: ['.js','.ts', 'tsx'],
   },
   // 出力するファイル
   output: {
