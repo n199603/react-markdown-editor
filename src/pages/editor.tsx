0d80a6400e281d0d9ed6347e7f6a76423d8568d4
Yuto Nakamura <yutonakamura0320@gmail.com>
2021-01-30 09:17:50 +0900

    feat(history): new component

--- src/pages/editor.tsx
+++ src/pages/editor.tsx
@@ -6,6 +6,7 @@ import { putMemo } from '../indexeddb/memos'
 import { Button } from '../components/button.tsx';
 import { SaveModal } from '../components/save_modal.tsx'
 import { Link } from 'react-router-dom';
+import { Header } from '../components/header.tsx'
 
 // useStateをReactから取り出す
 const { useState } = React
@@ -14,25 +15,25 @@ const { useState } = React
 const StorageKey = 'pages/editor:text'
 
 // スタイル
-const Header = styled.header`
-  align-content: center;
-  display: flex;
-  font-size: 1.5rem;
-  height: 2rem;
-  justify-content: space-between;
-  left: 0;
-  line-height: 2rem;
-  padding: 0.5rem 1rem;
-  position: fixed;
-  right: 0;
-  top: 0;
-`
+// const Header = styled.header`
+//   align-content: center;
+//   display: flex;
+//   font-size: 1.5rem;
+//   height: 2rem;
+//   justify-content: space-between;
+//   left: 0;
+//   line-height: 2rem;
+//   padding: 0.5rem 1rem;
+//   position: fixed;
+//   right: 0;
+//   top: 0;
+// `
 
-const HeaderControl = styled.div`
-  height: 2rem;
-  display: flex;
-  align-content: center;
-`
+// const HeaderControl = styled.div`
+//   height: 2rem;
+//   display: flex;
+//   align-content: center;
+// `
 
 const Wrapper = styled.div`
   bottom: 0;
@@ -40,6 +41,13 @@ const Wrapper = styled.div`
   position: fixed;
   right: 0;
   top: 3rem;
+`
+
+const HeaderArea = styled.div`
+  position: fixed;
+  right: 0;
+  top: 0;
+  left: 0;
 `
 
 const TextArea = styled.textarea`
@@ -84,17 +92,16 @@ export const Editor: React.FC = () => {
     // Reactは1つの要素を返却する必要がある
     // divでも良いが無駄なタグのため意味のないReact.Fragmentを使用する
     <>
-      <Header>
-        Markdown Editor
-        <HeaderControl>
+      <HeaderArea>
+        <Header title = "Markdown Editor">
           <Button onClick={() => setShowModal(true)}>
             Save
           </Button>
           <Link to = "/history">
             History
           </Link>
-        </HeaderControl>
-      </Header>
+        </Header>
+      </HeaderArea>
       <Wrapper>
         <TextArea
           // // テキストの内容が変更された時に実行される関数を渡す
--- src/pages/history.tsx
+++ src/pages/history.tsx
@@ -1,16 +1,55 @@
 import * as React from 'react'
-import { useHistory } from 'react-router-dom'
-import { Button } from '../components/button.tsx'
+// import { useHistory } from 'react-router-dom'
+// import { Button } from '../components/button.tsx'
+import {
+  Link,
+  useHistory,
+} from 'react-router-dom'
+import styled from 'styled-components'
+import { Header } from '../components/header.tsx'
+
+const HeaderArea = styled.div`
+  position: fixed;
+  right: 0;
+  top: 0;
+  left: 0;
+`
+
+const Wrapper = styled.div`
+  bottom: 0;
+  left: 0;
+  position: fixed;
+  right: 0;
+  top: 3rem;
+  padding: 0 1rem;
+`
+
+// export const History: React.FC = () => {
+//   const history = useHistory()
+//   return (
+//     <>
+//       <h1>History</h1>
+//       {/* history.push('遷移したいパス') */}
+//       <Button onClick = {() => history.push('/editor')}>
+//         Editor
+//       </Button>
+//     </>
+//   )
+// }
 
 export const History: React.FC = () => {
-  const history = useHistory()
   return (
     <>
-      <h1>History</h1>
-      {/* history.push('遷移したいパス') */}
-      <Button onClick = {() => history.push('/editor')}>
-        Editor
-      </Button>
+      <HeaderArea>
+        <Header title = "History">
+          <Link to = "/editor">
+            Back
+          </Link>
+        </Header>
+      </HeaderArea>
+      <Wrapper>
+        TODO: 履歴表示
+      </Wrapper>
     </>
   )
 }
